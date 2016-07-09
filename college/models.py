fee5ade6dbf5f40f5b7bf1c2759554b50d378c36
Abhishek Singhania <abhisinghania14@gmail.com>
2016-07-10 00:14:50 +0530

    College Table defined

--- college/models.py
+++ college/models.py
@@ -2,4 +2,13 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class College(models.Model):
+    name= models.CharField(max_length=100)
+    details = models.CharField(max_length=500)
+    link =models.CharField(max_length=100)
+    icon=models.ImageField(max_length=200)
+    coverImage=models.ImageField(max_length=200)
+
+    def __str__(self):
+    	return self.name
+
